5c828f1fd999b2bfc8d3eb58f4646e42cb4d11e4
oojas <oojasgupta@gmail.com>
2020-09-29 18:49:24 +0530

    Updation

--- src/tress/implementation.java
+++ src/tress/implementation.java
@@ -3,13 +3,14 @@ package tress;
 public class implementation {
 
 	public static void main(String[] args) {
-		int val;
-		val=20;
+//		int val;
+//		val=20;
 		Tree trees=new Tree();
 		trees.insert(35);
 		trees.insert(335);
 		trees.insert(20);
 		trees.insert(45);
+		trees.insert(65);
 
 	}
 
