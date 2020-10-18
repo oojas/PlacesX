c2d73c79b47ac340b6a75f00b87678d2079a7f8b
oojas <oojasgupta@gmail.com>
2020-10-18 09:00:11 +0530

    Addition of numbers

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -1,5 +1,25 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Recursion {
+        public static void main(String []args) {
+        	int n;
+        	Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        n=sc.nextInt();
+         System.out.print("The Sum is "+ sum(n));
+         sc.close();
+        }
         
+        static int sum(int n) {
+        	if(n==1) {
+        		return 1;
+        	}
+        	
+              return n+sum(n-1);
+
+        	
+        		
+        }
 }
