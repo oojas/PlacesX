6d71bc5ffc7a48618778eed25b27281f3e12624f
oojas <oojasgupta@gmail.com>
2020-10-18 20:08:24 +0530

    Added one problem

---
+++ src/recursion/fun.java
@@ -0,0 +1,22 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class fun {
+   public static void main(String [] agrs) {
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter the Number");
+	   int n=sc.nextInt();
+	   sc.close();
+	   foo(n);
+	   
+   }
+   static void foo(int n) {
+	   if(n==0) {
+		   return ;
+	   }
+	   System.out.println(n);
+	   foo(n-1);
+	   System.out.println(n);
+   }
+}
