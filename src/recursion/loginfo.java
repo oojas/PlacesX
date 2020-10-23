3dde15cd722413dc867ab56ce67361f3c7f31ddd
oojas <oojasgupta@gmail.com>
2020-10-23 14:41:48 +0530

    Added Log Recursion

---
+++ src/recursion/loginfo.java
@@ -0,0 +1,19 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class loginfo {
+    public static void main(String [] args) {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter the Number for whom you want to find the log");
+    	int n=sc.nextInt();
+    	int m=log(n);
+    	sc.close();
+    }
+   static int log(int n) {
+    	if(n==1) {
+    		return 0;
+    	}
+    	return 1 + log(n/2);
+    }
+}
