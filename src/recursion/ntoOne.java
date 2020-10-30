f8f737d3581a53c06599a2907525bf89ba2fe90b
oojas <oojasgupta@gmail.com>
2020-10-30 15:48:39 +0530

    Added N to one program

--- src/recursion/ntoOne.java
+++ src/recursion/ntoOne.java
@@ -1,10 +1,27 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class ntoOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		int m=1;
+		print(n,m);
+		sc.close();
+		
+		
+				
+	}
+	static void print(int n, int m) {
+		if(m>n) {
+			return ;
+		}
+		print(n,m+1);
+		System.out.print(m + " ");
 	}
 
 }
