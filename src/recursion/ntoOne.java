87425a63a868531e7024407021dbd24c7525eecc
oojas <oojasgupta@gmail.com>
2020-10-30 16:27:47 +0530

    Added comments

--- src/recursion/ntoOne.java
+++ src/recursion/ntoOne.java
@@ -12,16 +12,25 @@ public class ntoOne {
 		int m=1;
 		print(n,m);
 		sc.close();
-		
+		System.out.println();
+		print(n);
 		
 				
 	}
-	static void print(int n, int m) {
+	static void print(int n, int m) { // This is one approach
 		if(m>n) {
 			return ;
 		}
 		print(n,m+1);
-		System.out.print(m + " ");
+		System.out.print(m + " "); 
+	}
+	static void print(int n) { // This is a better approach because this is a tail recursion since the function
+		                          // has to do nothing after the recursion call
+		if(n==0) {
+			return;
+		}
+		System.out.print(n+ " ");
+		print(n-1);
 	}
 
 }
